4644ecea0780ca80e35a5d1cd2983d675710e419
Gerardo Del Angel <gerardoangeln@gmail.com>
2022-05-30 00:22:03 -0500

    Change files

--- truffle-config.js
+++ truffle-config.js
@@ -10,11 +10,11 @@ module.exports = {
       port: 8545,
       network_id: "*",
     },
-    rinkeby: {
+    kovan: {
       provider: function () {
         return new HDWalletProvider(process.env.PRIVATE_KEY, process.env.RINKEBY_RPC_URL);
       },
-      network_id: 4,
+      network_id: 42,
       skipDryRun: true,
     },
     mainnet: {
